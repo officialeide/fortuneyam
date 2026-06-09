10e0db09625a512f4f340fc7bc3ab08acbd3f33c
officialeide <official2die@gmail.com>
2026-06-09 11:22:16 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import SajuReport from './SajuReport.jsx'
 
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SajuReport />
+  </React.StrictMode>
+)
